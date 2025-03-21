0105c9da30a5c72f978213e7a260388b651c3327
bernardinoslv <bernardinoosilvaa@gmail.com>
2025-03-21 17:03:09 -0300

    update server.js: add middleware to session, session flash, auth middleware

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 require('dotenv').config()
 const express = require('express')
 const routes = require('./routes')
+const path = require('path')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require("express-flash")
 
 const app = express()
+
+// set engine/path views
+app.set("views", path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+// middlewares
+app.use(express.urlencoded({extended: false}))
+app.use(session({
+  secret: process.env.SECRET,
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore({
+    path: "./sessions"
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}))
+app.use(flash())
+app.use()
 
 app.use(routes)
 
