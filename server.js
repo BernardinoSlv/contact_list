6b2698bd13fa169188473ff6150534a050b1f994
bernardinoslv <bernardinoosilvaa@gmail.com>
2025-03-21 16:10:31 -0300

    add routes.js, server.js and package.json

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(routes)
+
+app.listen(3000, () => console.log("[ + ] online"))
