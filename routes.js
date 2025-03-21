e316139ddb12732651d27a9755fd3cdaab082ea5
bernardinoslv <bernardinoosilvaa@gmail.com>
2025-03-21 17:02:19 -0300

    update routes.js: add auth paths

--- routes.js
+++ routes.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const {checkLogin} = require('./src/middlewares/auth')
+const IndexController = require('./src/Controllers/IndexController')
+const AuthController = require('./src/Controllers/AuthController')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  res.send("Bem vindo")
-})
+// rotas auth
+routes.get('/auth', AuthController.index)
+routes.post('/auth', AuthController.attempt)
+
+// rotas index
+routes.get('/', checkLogin, IndexController.index)
 
 module.exports = routes
