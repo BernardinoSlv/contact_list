413d85272fa2cf104ae021c727fc3e6e58ebf1e6
bernardinoslv <bernardinoosilvaa@gmail.com>
2025-03-23 14:33:57 -0300

    update auth.js middleware: add message

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,9 @@
 exports.checkLogin = (req, res, next) => {
   if (!req.session?.user) {
-    return res.redirect('auth')
+    req.flash('message_type', "warning")
+    req.flash('message_text', "Faça login para acessar o painel")
+    res.writeHead(302, {Location: "/auth"})
+    return res.end()
   }
   next()
 } 
